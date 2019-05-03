508bd2b54661c07d2af5cacb5937a8604212c667
allenxuan <allenxuan@gmail.com>
2019-05-03 20:10:40 +0800

    update demo.

--- app/src/main/java/com/allenxuan/xuanyihuang/messagebox/demo/activities/DemoSubordinateActivity.kt
+++ app/src/main/java/com/allenxuan/xuanyihuang/messagebox/demo/activities/DemoSubordinateActivity.kt
@@ -2,6 +2,7 @@ package com.allenxuan.xuanyihuang.messagebox.demo.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.allenxuan.xuanyihuang.messagebox.R
 import com.allenxuan.xuanyihuang.messagebox.core.MessageBox
 
@@ -14,8 +15,21 @@ class DemoSubordinateActivity : AppCompatActivity() {
         initViews()
     }
 
-    private fun initViews(){
+    private fun initViews() {
         supportActionBar?.title = "Demo Subordinate Activity"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     override fun onDestroy() {
